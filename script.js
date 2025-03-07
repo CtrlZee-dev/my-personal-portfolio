46085c7fc448c8fb913b5ac68c1649897d581cf5
Zizipho Kakaza <142331580+CtrlZee-dev@users.noreply.github.com>
2025-03-07 15:20:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+window.onload = function () {
+  const loadingScreen = document.getElementById("loadingScreen");
+  if (loadingScreen) {
+    loadingScreen.style.display = "none"; // Hide the loading screen
+  }
+};
